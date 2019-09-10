67e0bd21667b5430d39c673fccd2aa0ac20c7860
liule1001 <1762219852@qq.com>
2019-09-10 22:09:22 +0800

    '1'

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
 import React from 'react';
-
+import 'style/test.less'
+import { Button } from 'antd';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      dvgyrgfyref
+      <Button type="primary">Primary</Button>
+      <Button type="link">Link</Button>
     </div>
   );
 }
